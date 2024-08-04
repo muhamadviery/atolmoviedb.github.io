48265399c1016aaa68782b566b64788935440e04
muhamadviery <69892076+muhamadviery@users.noreply.github.com>
2024-08-04 16:19:55 +0700

    Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/', 'Film::index');
+$routes->get('film/search', 'Film::search');
+$routes->get('film/detail/(:num)', 'Film::detail/$1');
+$routes->get('film/actor/(:num)', 'Film::actor/$1');
